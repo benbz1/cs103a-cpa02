82ff49dd6e58e0ebd07016fa838ec972d49ac2ea
Ben Bar Zvi <56666000+benbz1@users.noreply.github.com>
2022-05-05 12:54:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const courses = require('./public/data/courses20-21.json')
 const mongoose = require( 'mongoose' );
 //const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
 
-const mongodb_URI =  'mongodb+srv://ben:123@cluster0.v4e5f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongodb_URI = process.env.CONNECTION_STRING
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 
 // fix deprecation warnings
